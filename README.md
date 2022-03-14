74a0bcd6cbd14797d20b2a1868519b57da0777b6
Viktor Stanchev <me@viktorstanchev.com>
2022-03-14 02:46:24 +0000

    reduce benchmark overhead

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # dataloadgen
 
+Godoc: https://pkg.go.dev/github.com/vikstrous/dataloadgen
+
 This is a fork of https://github.com/vektah/dataloaden that uses generics
 instead of code genation. See the docs there for the motivation and usage details.
 
@@ -18,15 +20,15 @@ This package has evolved to include some ideas from dataloader https://github.co
 Benchmarks show that this package is faster than both of the above and offers the best of both worlds.
 
 ```
-BenchmarkDataloader/caches-8                     3963708               301.9 ns/op           162 B/op          4 allocs/op
-BenchmarkDataloader/random_spread-8               642184              1961 ns/op             736 B/op         14 allocs/op
-BenchmarkDataloader/concurently-8                  17191             85223 ns/op           45449 B/op        200 allocs/op
+BenchmarkDataloader/caches-8                     4753599               252.5 ns/op           147 B/op          3 allocs/op
+BenchmarkDataloader/random_spread-8               724077              1911 ns/op             758 B/op         17 allocs/op
+BenchmarkDataloader/concurently-8                  71977             21800 ns/op           10477 B/op        179 allocs/op
 
-BenchmarkDataloaden/caches-8                    10976748               108.6 ns/op            26 B/op          1 allocs/op
-BenchmarkDataloaden/random_spread-8              1000000              1107 ns/op             331 B/op          4 allocs/op
-BenchmarkDataloaden/concurently-8                  23605             53127 ns/op            2984 B/op         68 allocs/op
+BenchmarkDataloaden/caches-8                    17906731                65.20 ns/op           24 B/op          1 allocs/op
+BenchmarkDataloaden/random_spread-8              1253845               852.8 ns/op           277 B/op          5 allocs/op
+BenchmarkDataloaden/concurently-8                  25878             44226 ns/op            2807 B/op         75 allocs/op
 
-BenchmarkDataloadgen/caches-8                   12434064                97.04 ns/op           10 B/op          0 allocs/op
-BenchmarkDataloadgen/random_spread-8             1000000              1088 ns/op             307 B/op          3 allocs/op
-BenchmarkDataloadgen/concurently-8                 39688             30788 ns/op            2729 B/op         60 allocs/op
+BenchmarkDataloadgen/caches-8                   21556281                55.17 ns/op            8 B/op          0 allocs/op
+BenchmarkDataloadgen/random_spread-8             2498688               479.0 ns/op           289 B/op          4 allocs/op
+BenchmarkDataloadgen/concurently-8                 33300             34417 ns/op            2683 B/op         69 allocs/op
 ```
--- benchmarks_test.go
+++ benchmarks_test.go
@@ -42,9 +42,14 @@ func BenchmarkDataloader(b *testing.B) {
 	)
 
 	b.Run("caches", func(b *testing.B) {
+		queries := []IntKey{}
+		for n := 0; n < b.N; n++ {
+			queries = append(queries, IntKey(rand.Int()%300))
+		}
+		b.ResetTimer()
 		thunks := make([]func() (interface{}, error), b.N)
 		for i := 0; i < b.N; i++ {
-			thunks[i] = dl.Load(ctx, dataloader.StringKey(strconv.Itoa(rand.Int()%300)))
+			thunks[i] = dl.Load(ctx, queries[i])
 		}
 
 		for i := 0; i < b.N; i++ {
@@ -53,9 +58,14 @@ func BenchmarkDataloader(b *testing.B) {
 	})
 
 	b.Run("random spread", func(b *testing.B) {
+		queries := []IntKey{}
+		for n := 0; n < b.N; n++ {
+			queries = append(queries, IntKey(rand.Int()))
+		}
+		b.ResetTimer()
 		thunks := make([]func() (interface{}, error), b.N)
 		for i := 0; i < b.N; i++ {
-			thunks[i] = dl.Load(ctx, dataloader.StringKey(strconv.Itoa(rand.Int())))
+			thunks[i] = dl.Load(ctx, queries[i])
 		}
 
 		for i := 0; i < b.N; i++ {
@@ -64,44 +74,63 @@ func BenchmarkDataloader(b *testing.B) {
 	})
 
 	b.Run("concurently", func(b *testing.B) {
+		queries := []IntKey{}
+		for n := 0; n < b.N*10; n++ {
+			queries = append(queries, IntKey(rand.Int()))
+		}
+		b.ResetTimer()
 		var wg sync.WaitGroup
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
-			go func() {
+			go func(i int) {
 				for j := 0; j < b.N; j++ {
-					dl.Load(ctx, dataloader.StringKey(strconv.Itoa(rand.Int())))()
+					dl.Load(ctx, queries[i*b.N+j])
 				}
 				wg.Done()
-			}()
+			}(i)
 		}
 		wg.Wait()
 	})
 }
 
+// IntKey implements the Key interface for an int
+type IntKey int
+
+// String is an identity method. Used to implement String interface
+func (k IntKey) String() string { return strconv.Itoa(int(k)) }
+
+// String is an identity method. Used to implement Key Raw
+func (k IntKey) Raw() interface{} { return k }
+
 func BenchmarkDataloadgen(b *testing.B) {
-	dl := dataloadgen.NewLoader(func(keys []string) ([]benchmarkUser, []error) {
+	dl := dataloadgen.NewLoader(func(keys []int) ([]benchmarkUser, []error) {
 		users := make([]benchmarkUser, len(keys))
 		errors := make([]error, len(keys))
 
 		for i, key := range keys {
-			if rand.Int()%100 == 1 {
+			if key%100 == 1 {
 				errors[i] = fmt.Errorf("user not found")
-			} else if rand.Int()%100 == 1 {
+			} else if key%100 == 1 {
 				users[i] = benchmarkUser{}
 			} else {
-				users[i] = benchmarkUser{ID: key, Name: "user " + key}
+				users[i] = benchmarkUser{ID: strconv.Itoa(key), Name: "user " + strconv.Itoa(key)}
 			}
 		}
 		return users, errors
 	},
-		dataloadgen.WithBatchCapacity[string, benchmarkUser](100),
-		dataloadgen.WithWait[string, benchmarkUser](500*time.Nanosecond),
+		dataloadgen.WithBatchCapacity[int, benchmarkUser](100),
+		dataloadgen.WithWait[int, benchmarkUser](500*time.Nanosecond),
 	)
 
 	b.Run("caches", func(b *testing.B) {
+		queries := []int{}
+		for n := 0; n < b.N; n++ {
+			queries = append(queries, rand.Int()%300)
+		}
+		b.ResetTimer()
 		thunks := make([]func() (benchmarkUser, error), b.N)
 		for i := 0; i < b.N; i++ {
-			thunks[i] = dl.LoadThunk(strconv.Itoa(rand.Int() % 300))
+			thunks[i] = dl.LoadThunk(queries[i])
 		}
 
 		for i := 0; i < b.N; i++ {
@@ -110,9 +139,14 @@ func BenchmarkDataloadgen(b *testing.B) {
 	})
 
 	b.Run("random spread", func(b *testing.B) {
+		queries := []int{}
+		for n := 0; n < b.N; n++ {
+			queries = append(queries, rand.Int())
+		}
+		b.ResetTimer()
 		thunks := make([]func() (benchmarkUser, error), b.N)
 		for i := 0; i < b.N; i++ {
-			thunks[i] = dl.LoadThunk(strconv.Itoa(rand.Int()))
+			thunks[i] = dl.LoadThunk(queries[i])
 		}
 
 		for i := 0; i < b.N; i++ {
@@ -121,24 +155,29 @@ func BenchmarkDataloadgen(b *testing.B) {
 	})
 
 	b.Run("concurently", func(b *testing.B) {
+		queries := []int{}
+		for n := 0; n < 10*b.N; n++ {
+			queries = append(queries, rand.Int())
+		}
+		b.ResetTimer()
 		var wg sync.WaitGroup
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
-			go func() {
+			go func(i int) {
 				for j := 0; j < b.N; j++ {
-					dl.Load(strconv.Itoa(rand.Int()))
+					dl.Load(queries[j+i*b.N])
 				}
 				wg.Done()
-			}()
+			}(i)
 		}
 		wg.Wait()
 	})
 }
 func BenchmarkDataloaden(b *testing.B) {
-	dl := example.NewUserLoader(example.UserLoaderConfig{
+	dl := NewUserLoader(UserLoaderConfig{
 		Wait:     500 * time.Nanosecond,
 		MaxBatch: 100,
-		Fetch: func(keys []string) ([]*example.User, []error) {
+		Fetch: func(keys []int) ([]*example.User, []error) {
 			users := make([]*example.User, len(keys))
 			errors := make([]error, len(keys))
 
@@ -148,7 +187,7 @@ func BenchmarkDataloaden(b *testing.B) {
 				} else if rand.Int()%100 == 1 {
 					users[i] = nil
 				} else {
-					users[i] = &example.User{ID: key, Name: "user " + key}
+					users[i] = &example.User{ID: strconv.Itoa(key), Name: "user " + strconv.Itoa(key)}
 				}
 			}
 			return users, errors
@@ -156,9 +195,14 @@ func BenchmarkDataloaden(b *testing.B) {
 	})
 
 	b.Run("caches", func(b *testing.B) {
+		queries := []int{}
+		for n := 0; n < b.N; n++ {
+			queries = append(queries, rand.Int()%300)
+		}
+		b.ResetTimer()
 		thunks := make([]func() (*example.User, error), b.N)
 		for i := 0; i < b.N; i++ {
-			thunks[i] = dl.LoadThunk(strconv.Itoa(rand.Int() % 300))
+			thunks[i] = dl.LoadThunk(queries[i])
 		}
 
 		for i := 0; i < b.N; i++ {
@@ -167,9 +211,14 @@ func BenchmarkDataloaden(b *testing.B) {
 	})
 
 	b.Run("random spread", func(b *testing.B) {
+		queries := []int{}
+		for n := 0; n < b.N; n++ {
+			queries = append(queries, rand.Int())
+		}
+		b.ResetTimer()
 		thunks := make([]func() (*example.User, error), b.N)
 		for i := 0; i < b.N; i++ {
-			thunks[i] = dl.LoadThunk(strconv.Itoa(rand.Int()))
+			thunks[i] = dl.LoadThunk(queries[i])
 		}
 
 		for i := 0; i < b.N; i++ {
@@ -178,15 +227,20 @@ func BenchmarkDataloaden(b *testing.B) {
 	})
 
 	b.Run("concurently", func(b *testing.B) {
+		queries := []int{}
+		for n := 0; n < b.N*10; n++ {
+			queries = append(queries, rand.Int())
+		}
+		b.ResetTimer()
 		var wg sync.WaitGroup
 		for i := 0; i < 10; i++ {
 			wg.Add(1)
-			go func() {
+			go func(i int) {
 				for j := 0; j < b.N; j++ {
-					dl.Load(strconv.Itoa(rand.Int()))
+					dl.Load(queries[i*b.N+j])
 				}
 				wg.Done()
-			}()
+			}(i)
 		}
 		wg.Wait()
 	})
