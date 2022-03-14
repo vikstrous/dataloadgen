6ab4702b8cc612883302d96a32b19ac073635efe
Viktor Stanchev <me@viktorstanchev.com>
2022-03-14 01:00:44 +0000

    add test for cache edge case

--- dataloadgen_test.go
+++ dataloadgen_test.go
@@ -3,8 +3,11 @@ package dataloadgen_test
 import (
 	"fmt"
 	"strconv"
+	"sync"
+	"testing"
 	"time"
 
+	"github.com/stretchr/testify/require"
 	"github.com/vikstrous/dataloadgen"
 )
 
@@ -26,4 +29,45 @@ func ExampleLoader() {
 	}
 	fmt.Println(one)
 	// Output: 1
+}
+
+func TestEdgeCases(t *testing.T) {
+	var fetches [][]int
+	var mu sync.Mutex
+	dl := dataloadgen.NewLoader(func(keys []int) ([]string, []error) {
+		mu.Lock()
+		fetches = append(fetches, keys)
+		mu.Unlock()
+
+		results := make([]string, len(keys))
+		errors := make([]error, len(keys))
+
+		for i, key := range keys {
+			if key%2 == 0 {
+				errors[i] = fmt.Errorf("not found")
+			} else {
+				results[i] = fmt.Sprint(key)
+			}
+		}
+		return results, errors
+	},
+		dataloadgen.WithBatchCapacity[int, string](5),
+		dataloadgen.WithWait[int, string](1*time.Millisecond),
+	)
+
+	t.Run("load function called only once when cached", func(t *testing.T) {
+		for i := 0; i < 2; i++ {
+			_, err := dl.Load(0)
+			require.Error(t, err)
+			require.Len(t, fetches, 1)
+			require.Len(t, fetches[0], 1)
+		}
+		for i := 0; i < 2; i++ {
+			r, err := dl.Load(1)
+			require.NoError(t, err)
+			require.Len(t, fetches, 2)
+			require.Len(t, fetches[1], 1)
+			require.Equal(t, "1", r)
+		}
+	})
 }
